a0f9a026d6b7a4123206b2d0e1ff6ce79615f0e0
kate <suphatra1011@gmail.com>
2020-12-01 09:22:31 +0800

    To use makefile to build lib_hw

---
+++ ch3_libs/hw/hw_cmd_2/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "mylib.h"  
+
+int main() 
+{  
+    hello();  
+    world();  
+
+    func3();
+
+    return 0;
+}
---
+++ ch3_libs/hw/hw_makefile/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "../inc/mylib.h"  
+
+int main() 
+{  
+    hello();  
+    world();  
+
+    //func3();
+
+    return 0;
+}
